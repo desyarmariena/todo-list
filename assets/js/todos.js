2ce6a6622c06627004acb4d38e1aef4fbe11dc7d
Desy R. Armariena <desy.armariena@gmail.com>
2018-03-24 00:06:02 +0700

    Add remove function

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,10 @@
 $("li").click(function() {
     $(this).toggleClass("completed");
 });
+
+$("span").click(function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
