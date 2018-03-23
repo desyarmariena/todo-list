4d11b323b6222ca14062d8d852a2d34f7c133649
Desy R. Armariena <desy.armariena@gmail.com>
2018-03-24 00:07:37 +0700

    Add addition to the list function

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,10 +1,17 @@
-$("li").click(function() {
+$("ul").on("click", "li", function() {
     $(this).toggleClass("completed");
 });
 
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
     $(this).parent().fadeOut(500, function() {
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+    if (event.which === 13) {
+        $("ul").append("<li><span>X</span> " + $(this).val() + "</li>");
+        $(this).val("");
+    }
 });
